210dcb2951bfe1c1e5de50e96ba53c8e78435b50
Addison Reyes <106483642+AddisonReyes@users.noreply.github.com>
2023-04-09 21:22:58 -0400

    .

--- vrp_env/Scripts/activate.bat
+++ vrp_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Acrazt04\Documents\ITLA\6to cuatrimestre\Planificación inteligente\Proyecto VRP\vrp_env
+set VIRTUAL_ENV=C:\Users\Addison\Documents\GitHub\VRP_Planificacion_inteligente\vrp_env
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -25,6 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
+set VIRTUAL_ENV_PROMPT=(vrp_env) 
 
 :END
 if defined _OLD_CODEPAGE (
